a4eaf343d1404bec8ac03457cff5461420af48b6
zhangzheng <zhangzheng03@huatu.com>
2021-03-25 11:41:15 +0800

    first Commit

--- app/src/main/java/com/zz/verificationview/MainActivity.kt
+++ app/src/main/java/com/zz/verificationview/MainActivity.kt
@@ -3,16 +3,17 @@ package com.zz.verificationview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.zz.verificationview.VerificationCodeView.OnInputListener
+import com.zz.View.VerificationCodeView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        etView.setOnInputListener(object : OnInputListener{
+        etView.setOnInputListener(object : VerificationCodeView.OnInputListener {
             override fun onInput() {
-//                Toast.makeText(this@MainActivity, "输入中", Toast.LENGTH_SHORT).show()
+//                Toast.makeText(this@MainActivity, "", Toast.LENGTH_SHORT).show()
+
             }
 
             override fun complete(code: String?) {
@@ -20,8 +21,6 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-
-
 
     }
 }
